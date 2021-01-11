d157149b58d5d2fdaacfe741e340c61b824179b1
Carmen <cmarti@campuscamarafp.com>
2021-01-11 08:16:16 +0100

    tarea

--- src/entregable3/fecha.java
+++ src/entregable3/fecha.java
@@ -1,5 +1,12 @@
 package entregable3;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class fecha {
+	public static void main (String [] args) {
+		Date date = new Date();
+		DateFormat hourdateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		System.out.println(hourdateFormat.format(date));
+	}
 
 }
