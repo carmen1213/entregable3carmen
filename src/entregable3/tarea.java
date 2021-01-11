54f1efc1924c0404ab11ea9a2a9428bbec0f8f29
Carmen <cmarti@campuscamarafp.com>
2021-01-11 08:15:09 +0100

    fecha

--- src/entregable3/tarea.java
+++ src/entregable3/tarea.java
@@ -1,11 +1,11 @@
 package entregable3;
-
+import java.io.*;
+import java.util.Scanner;
 public class tarea {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
 	}
 
 }
